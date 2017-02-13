56a5b120d083a9ead726819bb28c3d8d05257c03
kristof verbeeck <kristofverbeeck@macbook-pro-van-kristof.local>
2017-02-13 01:18:08 +0100

    Initial Commit

---
+++ crypto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crypto
+//
+//  Created by kristof verbeeck on 13/02/17.
+//  Copyright © 2017 aaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
